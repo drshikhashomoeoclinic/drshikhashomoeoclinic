946401d7b07616c540df28819b68d1eb9c1d63d6
drshikhashomoeoclinic <shikha@drshikhashomoeoclinic.com>
2026-07-05 23:44:26 +0530

    Delete src directory

--- src/components/ui/PageLoader.jsx
+++
@@ -1,3 +0,0 @@
-export default function PageLoader() {
-  return <div className="grid min-h-screen place-items-center bg-white text-clinic-emerald">Loading clinic experience...</div>;
-}
--- src/pages/admin/AdminSeo.jsx
+++
@@ -1,5 +0,0 @@
-import AdminSettings from './AdminSettings.jsx';
-
-export default function AdminSeo() {
-  return <AdminSettings collectionName="settings" documentId="seo" title="SEO Manager" />;
-}
